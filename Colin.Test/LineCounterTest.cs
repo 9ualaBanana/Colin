8a319f66c47f7583383e5b62dac33d3f924a5b7a
GualaBanana <tem4rik@gmail.com>
2022-05-04 16:22:55 +0300

    Add LineCounter.

---
+++ Colin.Test/LineCounterTest.cs
@@ -0,0 +1,112 @@
+﻿using FluentAssertions;
+using System;
+using System.Linq;
+using System.IO;
+using Xunit;
+
+namespace Colin.Test;
+
+public class LineCounterTest
+{
+    const int numberOfFiles = 2;
+    const int numberOfLines = 10;
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    [InlineData(10)]
+    public void ApplyTo_File_ReturnsCorrectLineCount(int lineCount)
+    {
+        var sut = new LineCounter();
+        var path = Path.GetTempFileName();
+        var lines = new string[lineCount];
+        Array.Fill(lines, "a");
+        File.WriteAllLines(path, lines);
+
+        var result = sut.ApplyTo(path);
+
+        result.Should().Be(lineCount);
+    }
+
+    [Fact]
+    public void ApplyTo_Directory_AggregatesNestedLineCount()
+    {
+        var sut = new LineCounter();
+        var directoryPath = CreateTestingSystemEntries(numberOfLines);
+
+        var result = sut.ApplyTo(directoryPath);
+
+        result.Should().Be(numberOfLines * numberOfFiles);
+    }
+
+    [Fact]
+    public void ApplyTo_NonExistentFileSystemEntry_Throws()
+    {
+        var sut = new LineCounter();
+
+        sut.Invoking(sut => sut.ApplyTo(string.Empty)).Should().Throw<FileNotFoundException>();
+    }
+
+    [Fact]
+    public void ApplyToDirectory_TopDirectoryOnly_AggregatesNestedLineCount()
+    {
+        var sut = new LineCounter();
+
+        var path = CreateTestingSystemEntries(numberOfLines);
+
+        var result = sut.ApplyToDirectory(path, SearchOption.TopDirectoryOnly);
+
+        result.First().Should().Be(numberOfLines * numberOfFiles);
+    }
+
+    [Fact]
+    public void ApplyToDirectory_AllDirectories_DoesNotAggregateNestedLineCount()
+    {
+        var sut = new LineCounter();
+        var path = CreateTestingSystemEntries(numberOfLines);
+
+        var result = sut.ApplyToDirectory(path);
+
+        result.Count().Should().Be(numberOfFiles);
+    }
+
+    [Fact]
+    public void ApplyToDirectory_NonExistentPath_Throws()
+    {
+        var sut = new LineCounter();
+
+        sut.Enumerating(sut => sut.ApplyToDirectory(Path.GetTempFileName())).Should().Throw<DirectoryNotFoundException>();
+    }
+
+    [Fact]
+    public void ApplyToDirectory_FilePath_Throws()
+    {
+        var sut = new LineCounter();
+
+        sut.Enumerating(sut => sut.ApplyToDirectory(Path.GetTempFileName())).Should().Throw<DirectoryNotFoundException>();
+    }
+
+    /// <summary>
+    /// Creates top directory with two directories nested in each other. Also creates a file on each level except the top one (2 in total).
+    /// </summary>
+    /// <param name="numberOfLines">The number of lines to insert in files.</param>
+    /// <returns>The path to the top directory.</returns>
+    static string CreateTestingSystemEntries(int numberOfLines)
+    {
+        var topDirectoryPath = @".\Top";
+        var files = new string[2]
+        {
+            $@"{topDirectoryPath}\FirstLevel\TestData.txt",
+            $@"{topDirectoryPath}\FirstLevel\SecondLevel\TestData.txt"
+        };
+        Directory.CreateDirectory($@"{topDirectoryPath}\FirstLevel\SecondLevel");
+
+        var newLines = new string[numberOfLines];
+        Array.Fill(newLines, "a");
+        foreach (var file in files)
+        {
+            File.WriteAllLines(file, newLines);
+        }
+        return topDirectoryPath;
+    }
+}
---
+++ Colin/LineCounter.cs
@@ -0,0 +1,114 @@
+﻿using Colin.LineFilters;
+
+namespace Colin;
+
+/// <summary>
+/// Counts the number of lines in files taking into account optional <see cref="ILineFilter"/>s applied to them.
+/// </summary>
+/// <remarks>
+/// Can accept <see cref="ILineFilter"/>s to filter the lines that should be counted.
+/// </remarks>
+internal class LineCounter
+{
+    readonly ILineFilter _lineFilter;
+
+    internal LineCounter() : this(new LineFilter())
+    {
+    }
+
+    internal LineCounter(LineFilter lineFilter)
+    {
+        _lineFilter = lineFilter;
+    }
+
+    /// <summary>
+    /// Counts the number of lines in a system entry located at the provided <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to a file/directory to which it should be applied.</param>
+    /// <returns>The combined number of lines inside a system entry.</returns>
+    /// <exception cref="FileNotFoundException">File or directory was not found at <paramref name="path"/>.</exception>
+    internal int ApplyTo(string path)
+    {
+        if (File.Exists(path))
+        {
+            return ApplyToFile(path);
+        }
+        else if (Directory.Exists(path))
+        {
+            return ApplyToDirectory(path, SearchOption.TopDirectoryOnly).Aggregate(
+                seed: 0,
+                (totalLineCount, entryLineCount) => totalLineCount += entryLineCount
+                );
+        }
+        else throw new FileNotFoundException("The entry with the specified path was not found.", path);
+    }
+
+    /// <summary>
+    /// Counts the number of lines in files for each entry inside a directory located at <paramref name="path"/>.
+    /// </summary>
+    /// <remarks>
+    /// Recursively applied to each nested entry. <paramref name="searchOption"/> specifies whether the line count
+    /// of nested directories should be aggregated into a single value.<br/>
+    /// <see cref="SearchOption.TopDirectoryOnly"/> - aggregates; <see cref="SearchOption.AllDirectories"/> does not.
+    /// </remarks>
+    /// <param name="path">The path to the directory.</param>
+    /// <param name="searchOption"/><see cref="SearchOption.TopDirectoryOnly>"/> aggregates line count of nested directories into a single value.
+    /// <returns>The number of lines for each entry in the directory.</returns>
+    internal IEnumerable<int> ApplyToDirectory(string path, SearchOption searchOption = SearchOption.AllDirectories)
+    {
+        if (!Directory.Exists(path))
+        {
+            var exceptionMessage = File.Exists(path) ?
+                $"{nameof(path)} must refer to a directory and not a file." :
+                $"{nameof(path)} does not exist.";
+
+            throw new DirectoryNotFoundException(exceptionMessage);
+        }
+        foreach (var entry in Directory.EnumerateFileSystemEntries(path))
+        {
+            if (File.Exists(entry))
+            {
+                yield return ApplyToFile(entry);
+                continue;
+            }
+
+            if (searchOption is SearchOption.TopDirectoryOnly)
+            {
+                yield return ApplyToDirectory(entry).Aggregate(
+                    seed: 0,
+                    (totalLineCount, entryLineCount) => totalLineCount += entryLineCount
+                    );
+            }
+            else
+            {
+                foreach (var nestedDirectory in ApplyToDirectory(entry))
+                {
+                    yield return nestedDirectory;
+                }
+            }
+        }
+    }
+
+    int ApplyToFile(string path)
+    {
+        using var file = File.OpenRead(path);
+        return ApplyTo(file);
+    }
+
+    int ApplyTo(FileStream file)
+    {
+        using var sr = new StreamReader(file);
+        return ApplyTo(sr);
+    }
+
+    int ApplyTo(StreamReader file)
+    {
+        var lineCount = 0;
+        string? line;
+        while ((line = file.ReadLine()) is not null)
+        {
+            if (_lineFilter.IsPassedBy(line)) lineCount++;
+        }
+        return lineCount;
+    }
+}
